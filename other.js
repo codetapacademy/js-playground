b1cf60fbdcfd4bf35255ee12c3909df074122bfe
Marian Zburlea <marianzburlea@gmail.com>
2018-08-02 15:39:00 +0100

    After a nice sweat of wtf and why and so many errors: Eureka

---
+++ other.js
@@ -0,0 +1,11 @@
+const other = (s = '') => console.log(s + ' this comes from the other.js file');
+export default other;
+// module.exports = {
+//   // otherExported: other
+//   // asdasd: () => other('Uuuuu!')
+//   // other: other
+//   other
+// }
+
+// other();
+// module.exports.other()
